1cacd6037743bc937ba326b5f96123de9b3f0f71
gustavo acuna <gustavo.acu.rivas@gmail.com>
2024-05-28 13:46:43 -0400

    added dashboard service

--- src/app/Services/dashboard.service.ts
+++ src/app/Services/dashboard.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ResponseApi } from '../Interfaces/response-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  private urlEndpoint: string = environment.endpoint.concat("dashboard");
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetDashboard (): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${ this.urlEndpoint }/summary`)
+  }
 }
