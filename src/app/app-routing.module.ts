bdc48d2f8d4aef22f2909cf78a331e8f70c8b0de
gustavo acuna <gustavo.acu.rivas@gmail.com>
2024-05-28 15:10:39 -0400

    added app module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { LayoutComponent } from './Components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:LoginComponent, pathMatch: 'full' },
+  { path: 'login', component:LoginComponent, pathMatch: 'full' },
+
+  // A continuacion se aplicará lazy loading de los componenetes, es decir, se cargarán cuando se les llama
+  {
+    path: 'pages',
+    loadChildren: () => import("./Components/layout/layout.module")
+      .then(x => x.LayoutModule)
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
