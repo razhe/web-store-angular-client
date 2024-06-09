48117edc5b419a86425043861846acb71d5658e6
gustavo acuna <gustavo.acu.rivas@gmail.com>
2024-06-09 17:58:39 -0400

    added dashboard module

--- src/app/Components/layout/Pages/dashboard/dashboard.component.ts
+++ src/app/Components/layout/Pages/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { SaleService } from '../../../../Services/sale.service';
+import { Y } from '@angular/cdk/keycodes';
+import { DashboardService } from '../../../../Services/dashboard.service';
+import { AssetsService } from '../../../../Utils/assets.service';
+
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +13,58 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  totalProfit: string = "0";
+  totalSales: number = 0;
+  totalProducts: number = 0;
 
+
+  constructor(
+    private dashboardService: DashboardService,
+    private assetService: AssetsService
+  ) {
+    this.dashboardService.GetDashboard().subscribe({
+      next: (response) => {
+        if (response.data !== null) {
+          this.totalProfit = response.data.totalProfit;
+          this.totalSales = response.data.totalSales;
+          this.totalProducts = response.data.totalProducts;
+
+          const arrayData: any[] = response.data.lastWeekSales;
+
+          const labelTemp = arrayData.map((value) => value.date);
+          const dataTemp = arrayData.map((value) => value.total);
+
+          this.showGraphic(labelTemp, dataTemp);
+        }
+      },
+      error: (response: any) => {
+        this.assetService.showAlert(
+          response.error.message ??
+          "Ha ocurrido un error inesperado, intente nuevamente. Si persiste comuniquese con sorporte", "Oops!")
+      }
+    })
+  }
+
+  showGraphic(label: any[], data: any[]) {
+    const chartBars = new Chart('chartBars', {
+      type: 'bar',
+      data: {
+        labels: label,
+        datasets: [{
+          label: "# Sales",
+          data: data,
+          backgroundColor: [ 'rgba(54, 162, 235, 0.2)' ],
+          borderColor: [ 'rgba(54, 162, 235, 1)' ],
+          borderWidth: 1
+        }],
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          Y: { beginAtZero: true }
+        }
+      }
+    });
+  }
 }
